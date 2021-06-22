13b97860df7dd50073bba5b43de8c0cffa9ec905
sfgesg <sammiemagalong@gmail.com>
2021-06-22 15:48:43 +0800

    title

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
  <html lang="en" class="no-js">  
  <head>  
         <meta charset="UTF-8" />  
-        <title>Login and Registration Form with HTML5 and CSS3</title>  
+        <title>Login and Registration Form</title>  
         <meta name="viewport" content="width=device-width, initial-scale=1.0">   
         <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />  
         <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />  
